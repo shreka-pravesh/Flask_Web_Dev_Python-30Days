23535ef0fc2094bc9b9d44468bc90ba67deee208
shreka-pravesh <shrekadhanu@gmail.com>
2025-11-17 22:45:06 +0530

    Create visit_counter.py
    Shows how many times the browser visited using a cookie.

---
+++ Day12_Sessions_&_Cookies/visit_counter.py
@@ -0,0 +1,8 @@
+<html>
+<head><title>Visit Counter (Cookie)</title></head>
+<body>
+  <h2>Visit Counter (cookie)</h2>
+  <p>You visited this page <b>{{ visits }}</b> time(s).</p>
+  <p><a href="/reset">Reset counter</a></p>
+</body>
+</html>
